540d9d966e022a8ac097525bbbe8bd785303969a
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-04-02 23:08:25 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(listName, event) {
+    let newArr = [];
+    for(let i = 0; i < listName.length; i++) {
+        newArr.push(`Thank you, ${listName[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
